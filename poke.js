d1bef27e1ae32c06bd67a766a6ed5c8e5d7c670a
Elisej0072 <Elise22x08@gmail.com>
2025-12-12 11:48:35 -0500

    5

---
+++ poke.js
@@ -0,0 +1,19 @@
+async function getData(){
+    try {
+        //go get data
+        const response = await fetch (`link ${poke}`);
+        // handle errors
+        if(response.status != 200){
+
+            throw new Error(response)}
+        else {
+            //makes the response into json data we can use
+            const data = await response.json();
+            console.log(data);
+            document.getElementById("api-response").textContent = data.name;
+            }
+    } catch (error) {
+     console.log (error);
+    }
+}
+getData(insert_);
