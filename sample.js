3d6b48dd7ae1cd9e55d0957fe5fc221b7ff6d3fc
Elisej0072 <Elise22x08@gmail.com>
2026-01-06 12:30:43 -0500

    3

--- sample.js
+++ sample.js
@@ -4,22 +4,29 @@ publishers = {};
 videoGames.forEach((game) => {
   let studio = game.studio;
   if(!publishers[studio]) {
-    publishers[studi] = 1;
+    publishers[studio] = 1;
   }else{
     publishers[studio] = publishers[studio] + 1;
   }
   });
-   //console.log(publishers)
-for(let publisher in publishers){
-    let publishDiv = document.querySelector(".publishers");
-    publishDiv.insertAdjacentHTML (
-         "afterbegin",
-         `<div className = "publisher-container>`
-    <h2 className="publisher"> ${publisher}</h2>
-    <h3 className="amount">${publishers[publisher}]} </h3>
-    </div>);} 
 
-videogames.filter((game) => if(game.ratings.metacritic > 95).forEach((game)
-=> document.querySelector(".games")
-            .insertAdjacentHTML("afterbegin", `<h2>${game.title}</h2>`))
-        );
+   //console.log(publishers)
+// for(let publisher in publishers){
+//     let publishDiv = document.querySelector(".publishers");
+//     publishDiv.insertAdjacentHTML (
+//          "afterbegin",
+//          `<div className = "publisher-container>`
+//     <h2 className="publisher"> ${publisher}</h2>
+//     <h3 className="amount">${publishers[publisher}]} </h3>
+//     </div>);} 
+
+// videogames.filter((game) => if(game.ratings.metacritic > 95).forEach((game)
+// => document.querySelector(".games")
+//             .insertAdjacentHTML("afterbegin", `<h2>${game.title}</h2>`))
+//         );
+
+
+console.log(companies)
+for(let company in companies){
+  let o
+}
